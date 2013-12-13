0e781cedcf3bb711c1e895b765f71eb491fa0d37
Dee Jordan <dee@red-staple.com>
2013-12-12 23:50:38 -0800

    caching all posts on index page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,16 @@
 class Post < ActiveRecord::Base
   has_many :comments
   accepts_nested_attributes_for :comments
+
+  after_commit :flush_cache
+
+
+  def self.cached_posts
+    Rails.cache.fetch('all_posts') {Post.all.to_a}
+  end
+
+  def flush_cache
+    Rails.cache.delete('all_posts')
+  end
+
 end
