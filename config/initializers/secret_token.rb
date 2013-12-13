2ae8f091ae04a6013e20cf8bb0e3b995686a53a3
Dee Jordan <dee@red-staple.com>
2013-12-12 21:57:38 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cachetest::Application.config.secret_key_base = 'bfbdbc4a26efc3761040a0dbbefaed679ad7f2fca57089cfbd4b6da49235735013bae801dd9d6ea444efcd11b0566f2df748c3699ef2be902d6146b376f1122b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cachetest::Application.config.session_store :cookie_store, key: '_cachetest_session'
